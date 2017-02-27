d266ecc6e49b5d4c47f8d6e58bdb132dd0cead04
chrisgaber <chrisgaber@gmail.com>
2017-02-27 08:48:07 -0800

    Major Update
    Added uMirror and uMapper.  Reworked uluroVariable to work with uMirror.
    uluroGroup is now unchecked.

--- UluroCreate/uluroclasses.cs
+++ UluroCreate/uluroclasses.cs
@@ -1,78 +1,74 @@
 ﻿using System;
 using System.IO;
 
+public class uMirror
+{
+	private StreamWriter _outFile;
+	private StreamReader _mirrorFrom;
+	private string _insertTag;
+	public uMirror(string getFrom, string GroupName, StreamWriter outFile)
+	{
+		_mirrorFrom = new StreamReader(getFrom);
+		_insertTag = GroupName;
+		_outFile = outFile;
+	}
+
+	public void setGroup(string GroupName)
+	{
+		_insertTag = GroupName;
+	}
+
+	public void mirrorToGroup()
+	{
+		string line = "";
+		while(!_mirrorFrom.EndOfStream && !(line = _mirrorFrom.ReadLine()).StartsWith("C019\t\""+_insertTag))
+		{
+			_outFile.WriteLine(line);
+		}
+
+		if (!_mirrorFrom.EndOfStream)
+			_outFile.WriteLine(line); //Write the tag itself.
+	}
+
+	public void mirrorAfterGroup()
+	{
+		string line = "";
+		while (!_mirrorFrom.EndOfStream && !(line = _mirrorFrom.ReadLine()).StartsWith("C020\t\"" + _insertTag))
+		{
+			continue;
+		}
+
+		_outFile.WriteLine(line);
+
+		while (!_mirrorFrom.EndOfStream)
+		{
+			line = _mirrorFrom.ReadLine();
+			_outFile.WriteLine(line);
+		}
+	}
+}
 
 public class uluroGroup
 {
-    /*
-    Open Group
-    C020	"headerName"	N	N	N
-    Close Group
-    C020	"headerName"	N	N	N
-     */
-    private string groupName;
-    //{
-    //    get
-    //    {
-    //        return groupName;
-    //    }
-    //    set
-    //    {
-    //        string test = value;
-    //        if (test.Substring(0, 1) != "\"")
-    //        {
-    //            test = '\"' + test;
-    //        }
-    //        if (test.Substring(test.Length - 1) != "\"")
-    //        {
-    //            test = test + '\"';
-    //        }
 
-    //        this.groupName = test;
-
-    //    }
-    //}
-    public bool openState { get; set; }
     public StreamWriter outFile { get; set; }
 
-    public uluroGroup(string name, StreamWriter outPut)
+    public uluroGroup(StreamWriter outPut)
     {
-        groupName = "\"" + name + "\"";
         outFile = outPut;
-        openState = true;
     }
 
-    public void writeOpen()
+    public void writeOpen( string name)
     {
 
-        //flip the state and header to be written.
-        if (openState)
-        {
-            openState = false;
-        }
-        else
-        {
-            Console.WriteLine("Another instance of uluroGroup " + this.groupName + " was opened before the previous was closed.");
-        }
-
-        outFile.WriteLine("C019" + '\t' + this.groupName + "	N	N	N");
+        outFile.WriteLine("C019" + "\t\"" + name + "\"	N	N	N");
 
 	}
 
-    public void writeClose()
-    {
+	public void writeClose(string name)
+	{
 
-        //flip the state and header to be written.
-        if (!openState)
-        {
-            openState = true;
-        }
-        else
-        {
-            Console.WriteLine("uluroGroup " + this.groupName + " was closed when it was already closed.");
-        }
-
-        outFile.WriteLine("C020" + '\t' + this.groupName + "	N	N	N");
+		outFile.WriteLine("C020" + "\t\"" + name + "\"	N	N	N");
 
 	}
 }
@@ -95,11 +91,12 @@ public class uluroCond
                 testCode = "\"E\"";
                 break;
 			case "NOT BLANK":
-				testCode = "\"X\"";
+			case "NOT EMPTY":
+				testCode = "\"0\"";
 				break;
 			case "BLANK":
             case "EMPTY":
-                testCode = "\"0\"";
+                testCode = "\"X\"";
                 break;
             default:
                 testCode = "\"" + test + "\"";
@@ -108,7 +105,7 @@ public class uluroCond
         return testCode;
     }
 
-    private void writeCondition<T, V>(T param1, string test, V param2, bool NOT=false)
+    private void writeCondition<T, V>(T param1, string test, V param2, bool isTrue)
     {
         //C012	""	"E"	C2	F	ACCT_ID	X	\d{4,6}-\d{4,6}	N	Y/N	N
         string outString = "C012	\"\"	" + getCondValue(test);
@@ -116,36 +113,86 @@ public class uluroCond
         outString = outString + "\tC2\t" + (param1.GetType() == System.Type.GetType("String") ? "X\t" + param1:"V\t" + param1.ToString()) 
             + "\t" 
             + (param2.GetType() == System.Type.GetType("String") ? "X\t" + param2 : "V\t" + param2.ToString()) 
-            + "\tN\t" + (NOT ? "Y" : "N") + "\tN";
+            + "\tN\t" + (isTrue ? "N" : "Y") + "\tN";  //Counter intuitive, but this is for the NOT check box.
         outFile.WriteLine(outString);
     }
     
     //Maybe an easier way to to do this.
-    public void createCondition(uluroVariable param1, string test, uluroVariable param2, bool NOT=false)
+    public void createCondition(uluroVariable param1, string test, uluroVariable param2, bool isTrue)
     {
-        writeCondition<uluroVariable, uluroVariable>(param1, test, param2, NOT);
+        writeCondition<uluroVariable, uluroVariable>(param1, test, param2, isTrue);
     }
-    public void createCondition(string param1, string test, uluroVariable param2, bool NOT = false)
+    public void createCondition(string param1, string test, uluroVariable param2, bool isTrue)
     {
-        writeCondition<string, uluroVariable>(param1, test, param2, NOT);
+        writeCondition<string, uluroVariable>(param1, test, param2, isTrue);
     }
-    public void createCondition(uluroVariable param1, string test, string param2, bool NOT = false)
+    public void createCondition(uluroVariable param1, string test, string param2, bool isTrue)
     {
-        writeCondition<uluroVariable, string>(param1, test, param2, NOT);
+        writeCondition<uluroVariable, string>(param1, test, param2, isTrue);
     }
-    public void createCondition(string param1, string test, string param2, bool NOT = false)
+    public void createCondition(string param1, string test, string param2, bool isTrue)
     {
-        writeCondition<string, string>(param1, test, param2, NOT);
+        writeCondition<string, string>(param1, test, param2, isTrue);
     }
-    public void createCondition(string param1, string test, bool NOT = false)
+    public void createCondition(string param1, string test, bool isTrue)
     {
-        writeCondition<string, string>(param1, test, "", NOT);
+        writeCondition<string, string>(param1, test, "", isTrue);
     }
-    public void createCondition(uluroVariable param1, string test, bool NOT = false)
+    public void createCondition(uluroVariable param1, string test, bool isTrue)
     {
-        writeCondition<uluroVariable, string>(param1, test, "", NOT);
+        writeCondition<uluroVariable, string>(param1, test, "", isTrue);
     }
 
+}
+
+public class uluroFuncWriter
+{
+	StreamWriter outFile;
+
+	public uluroFuncWriter(StreamWriter outFile)
+	{
+		this.outFile = outFile;
+	}
+
+	private void writeFunc(uluroVariable assignTo, string func, uluroVariable[] varParams=null, string[] fixedParams=null)
+	{
+		int varCount, fixCount, paramTot;
+		varCount = (varParams == null ? 0 : varParams.Length);
+		fixCount = (fixedParams == null ? 0 : fixedParams.Length);
+		paramTot = varCount + fixCount;
+		//"C011	\"YPOS\"	\"R\"	\"+\"	C2	X	.3	V	YPOS	N	N"
+		//C011	"resultvar"	"resultType"	"+" C(paramcount)	X/V VAL/PARAMNAME	N	N
+
+		string outStr = "C011" + "	\"" +
+			assignTo.varName + "\"	\"" + assignTo.varType + "\"	\""
+			+ func + "\"	C" + varCount.ToString() + '\t';
+
+		for(int i = 0; i < varCount; i ++)
+		{
+			outStr = outStr + "V	" + varParams[i].varName + '\t';
+		}
+
+		for (int i = 0; i < fixCount; i++)
+		{
+			outStr = outStr + "X	" + fixedParams[i] + '\t';
+		}
+		outFile.WriteLine(outStr + "N	N");
+	}
+
+	public void createFunc(uluroVariable assignTo, string func, params uluroVariable[] varParams)
+	{
+		writeFunc(assignTo, func, varParams,null);
+	}
+
+	public void createFunc(uluroVariable assignTo, string func, params string[] fixParams)
+	{
+		writeFunc(assignTo, func, null, fixParams);
+	}
+
+	public void createFunc(uluroVariable assignTo, string func, uluroVariable[] varParams, string[] fixedParams)
+	{
+		writeFunc(assignTo, func, varParams, fixedParams);
+	}
 }
 
 public abstract class uluroVariable
@@ -259,38 +306,75 @@ public abstract class uluroVariable
         outFile.WriteLine(outStr);
     }
 
-    //Fixed
-    public void createMap(float xpos, float ypos, string format = "")
-    {
-		//C009	0	3.0520	4.3960	"_TEST CURRENCY"	1		Y		L	Y	Y	N	N	N	0									Y
-		string outStr = "C009\t0\t";
-		outStr = outStr + xpos.ToString("F") + "\t" + ypos.ToString() + "\t\"" + varName + "\"";
-		//This part is static.  1 is tied to the var num of the variable, but doesn't seem to matter.
-		//special formatting goes at the first Y spot.
-		outStr = outStr + "	1	" + format + "	Y		L	Y	Y	N	N	N	0									Y";
-		
-		outFile.WriteLine(outStr);
-	}
-	
-	//dynamic
-	public void createMap(uReal xpos, uReal ypos, string format = "")
+	private string setFont(string alignment = "L", string fontName = "Arial", string fontSize = "10", string fontStyle = "")
 	{
-		//C009	0	3.0520	4.3960	"_TEST CURRENCY"	1		Y		L	Y	Y	N	N	N	0	V	DESCR X	Y POSITION						Y
-		string outStr = "C009\t0\t0.0000\t0.0000\t\"" + varName + "\"";
-		//This part is static.  1 is tied to the var num of the variable, but doesn't seem to matter.
-		//special formatting goes at the first Y spot.
-		outStr = outStr + "	1	"+format+"	Y		L	Y	Y	N	N	N	0";
-		outStr = outStr + "	V	" + xpos.varName + "	" + ypos.varName;
-		//Not sure what this is.
-		outStr = outStr + "						Y";
-		outFile.WriteLine(outStr);
+		//N	"Arial"	8	TY	0	"BOLD ITALIC UNDERLINE STRIKEOUT "	L
+		string fontString = "N" + "	\"" + fontName + "\"	" + fontSize + "	TY	0	\"" + fontStyle + "\"	" + alignment;
+
+		return fontString;
 	}
 
-	public void setFont()
+	private void genericMapWriter(float xpos, float ypos, string varName, string format, string fontstring, string varString = "			")
 	{
-		//To be worked on in the future.
+		//C009	0	3.0520	4.3960	"_TEST CURRENCY"	1		Y		L	Y	Y	N	N	N	0	V	DESCR X	Y POSITION						Y
+		string genericFormula = "C009\t0\t" + xpos.ToString() + "\t" + ypos.ToString() + "\t\"" + varName + "\"";
+		//This part is static.  1 is tied to the var num of the variable, but doesn't seem to matter.
+		//special formatting goes at the first Y spot.
+		genericFormula = genericFormula + "	1	" + format +'\t'+ fontstring + "	Y	Y	N	N	N	0" + varString + "						Y";
+		outFile.WriteLine(genericFormula);
 	}
-   
+
+	public void createMap(uReal xpos, uReal ypos, string format = "", string alignment = "L", string fontName = "Arial", string fontSize = "10", string fontStyle = "")
+	{
+		string fontStr = setFont(alignment, fontName, fontSize, fontStyle);
+		string varStr = "	V	" + xpos.varName + "	" + ypos.varName;
+		genericMapWriter(0.0000f, 0.0000f, this.varName, format, fontStr, varStr);
+	}
+
+	public void createMap(float xpos, float ypos, string format = "", string alignment = "L", string fontName = "Arial", string fontSize = "10", string fontStyle = "")
+	{
+		string fontStr = setFont(alignment, fontName, fontSize, fontStyle);
+		genericMapWriter(xpos, ypos, this.varName, format, fontStr);
+	}
+
+}
+
+//This holds font info and maps variables to it.  Its possible to call the mapper directly on the uVariables if desired.  Just repetative.
+public class uMapper
+{
+	private string _fontName;
+	private string _fontSize;
+	private string _fontStyle;
+	private string _alignment;
+
+	public uMapper(string alignment = "L", string fontName = "Arial", string fontSize = "10", string fontStyle = "")
+	{
+		_alignment = alignment;
+		_fontName = fontName;
+		_fontSize = fontSize;
+		_fontStyle = fontStyle.ToUpper();
+	}           
+
+	public void resetFonts(string alignment = "L", string fontName = "Arial", string fontSize = "10", string fontStyle = "")
+	{
+		_alignment = alignment;
+		_fontName = fontName;
+		_fontSize = fontSize;
+		_fontStyle = fontStyle.ToUpper();
+	}
+
+
+
+	public void createMap(uluroVariable mapThis, uReal xpos, uReal ypos, string format = "")
+	{
+		mapThis.createMap(xpos, ypos, format, _alignment, _fontName, _fontSize, _fontStyle);
+	}
+
+
+	public void createMap(uluroVariable mapThis, float xpos, float ypos, string format = "")
+	{
+		mapThis.createMap(xpos, ypos, format, _alignment, _fontName, _fontSize, _fontStyle);
+	}
 }
 
 public class uDate : uluroVariable
--- UluroCreate/ulurocreator.cs
+++ UluroCreate/ulurocreator.cs
@@ -9,58 +9,159 @@ namespace UluroCreate
 {
     class program
     {
+
         static void Main(string[] args)
         {
 
-            StreamWriter outTemp = new StreamWriter(@"c:\temp\testout.txt");
+            StreamWriter outTemp = new StreamWriter(@"c:\temp\testout.nmp");
+
+			uMirror da3 = new uMirror(@"U:\SUBMIT\MAPS\DACDA3\da3.nmp", "DETAILS",outTemp);
+
+			da3.mirrorToGroup();
+
             Dictionary<string, uText> tranLines = new Dictionary<string, uText>();
             for (int i = 0; i < 5; i++)
             {
                 string descr = "PROP CLASS " + (i + 1).ToString();
                 tranLines.Add(descr, new uText(descr, outTemp));
-                tranLines[descr].createReadPosition(xpos: 7392 + i * 65, length: 30, funcName: "TRIM");
+                //tranLines[descr].createReadPosition(xpos: 7392 + i * 65, length: 30, funcName: "TRIM");
 
                 string actual = "CURR ACTUAL " + (i + 1).ToString();
                 tranLines.Add(actual, new uText(actual, outTemp));
-                tranLines[actual].createReadPosition(xpos: 7422 + i * 65, length: 15, funcName: "TRIM");
+                //tranLines[actual].createReadPosition(xpos: 7422 + i * 65, length: 15, funcName: "TRIM");
 
                 string taxable = "CURR TAXABLE " + (i + 1).ToString();
                 tranLines.Add(taxable, new uText(taxable, outTemp));
-                tranLines[taxable].createReadPosition(xpos: 7437 + i * 65, length: 15, funcName: "TRIM");
+                //tranLines[taxable].createReadPosition(xpos: 7437 + i * 65, length: 15, funcName: "TRIM");
             }
 
-
+			//Mapping part 1
             uluroCond test = new uluroCond(outTemp);
-            uluroGroup wraper = new uluroGroup("COND MAP",outTemp);
-			uReal xPos1 = new uReal("XPOS1", outTemp);
-			uReal xPos2 = new uReal("XPOS2", outTemp);
-			uReal xPos3 = new uReal("XPOS3", outTemp);
-			xPos1.createFixed("1");
-			xPos2.createFixed("2");
-			xPos3.createFixed("3");
+			uluroFuncWriter ufunc = new uluroFuncWriter(outTemp);
+			uluroGroup group = new uluroGroup(outTemp);
+
+			uMapper col1 = new uMapper(alignment: "R", fontSize: "8", fontStyle: "BOLD ");
+			uMapper col2 = new uMapper(alignment: "L", fontSize: "8");
+			uMapper col3 = new uMapper(alignment: "C", fontSize: "8");
+
 			uReal ypos = new uReal("YPOS", outTemp);
-			ypos.createFixed("4");
+			uReal ydelta = new uReal("YDELTA", outTemp);
+
+			ypos.createFixed("4.35");
+			ydelta.createFixed(".175");
 			
-            for (int i=0;i<5;i++)
+			//Mapping part 1
+			group.writeOpen("TOP TRANS");
+			uReal xTop1 = new uReal("XTOP1", outTemp);
+			uReal xTop2 = new uReal("XTOP2", outTemp);
+			xTop1.createFixed("1.4");
+			xTop2.createFixed("1.5");
+			string[] outDescr = { "Account Type:", "Neighborhood:", "Situs Address:", "Legal Descr:" };//, "Zoning:", "TotalAcreage:"};
+			string[] outVal = { "_ACCT TYPE", "_NEIGHBORHOOD", "_SITUS", "_LEGAL LINE 1" };//, "_PROPERTY USE"};
+			for (int i = 0; i < outDescr.Length; i++)
+			{
+				uText udescr = new uText(outDescr[i].Replace(":", ""), outTemp);
+				uText uVal = new uText(outVal[i], outTemp);
+
+				test.createCondition(uVal.varName, "not EMPTY",true);
+				group.writeOpen("MAP " + outVal[i]);
+
+				udescr.createFixed(outDescr[i]);
+				col1.createMap(udescr,xTop1, ypos);
+				col2.createMap(uVal,xTop2, ypos);
+				ufunc.createFunc(ypos, "+", ypos, ydelta);
+				group.writeClose("MAP " + outVal[i]);
+				
+			}
+			group.writeClose("TOP TRANS");
+
+			//Mapping Part 2
+			ufunc.createFunc(ypos, "+", ypos, ydelta);//Extra space
+			uReal xMid1 = new uReal("XMID1", outTemp);
+			uReal xMid15 = new uReal("XMID1.5", outTemp);
+			uReal xMid2 = new uReal("XMID2", outTemp);
+			uReal xMid3 = new uReal("XMID3", outTemp);
+			xMid1.createFixed("1.75");
+			xMid15.createFixed("3.5");
+			xMid2.createFixed("6.15");
+			xMid3.createFixed("7.56");
+
+			group.writeOpen("MID TRANS");
+			
+			col1.resetFonts(alignment: "L", fontSize: "8");
+			col2.resetFonts(alignment: "C", fontSize: "8");
+			col3.resetFonts(alignment: "C", fontSize: "8");
+			for (int i=0;i<5;i++)
             {
-                test.createCondition(tranLines["PROP CLASS " + (i + 1).ToString()], "blank",false);
-                wraper.writeOpen();
+                test.createCondition(tranLines["PROP CLASS " + (i + 1).ToString()], "not blank",true);
+                group.writeOpen("PROP CLASS " + (i + 1).ToString());
 
 				string descr = "PROP CLASS " + (i + 1).ToString();
-				tranLines[descr].createMap(xPos1, ypos);
+				col1.createMap(tranLines[descr],xMid1, ypos);
 
 				string actual = "CURR ACTUAL " + (i + 1).ToString();
-				tranLines[actual].createMap(xPos2, ypos);
+				col2.createMap(tranLines[actual], xMid2, ypos);
 
 				string taxable = "CURR TAXABLE " + (i + 1).ToString();
-				tranLines[taxable].createMap(xPos3,ypos);
-				outTemp.WriteLine("C011	\"YPOS\"	\"R\"	\" + \"	C2	X	.3	V	YPOS	N	N");
-				wraper.writeClose();
+				col3.createMap(tranLines[taxable], xMid3, ypos);
+				group.writeClose("PROP CLASS " + (i + 1).ToString());
 
             }
+
+			ufunc.createFunc(ypos, "+", ypos, ydelta);//Extra space
+
+			uText currenttotal = new uText("SUBTOTAL",outTemp);
+			currenttotal.createFixed("Current Year’s Total Value");
+			col1.createMap(currenttotal, xMid1, ypos);
+			ufunc.createFunc(ypos, "+", ypos, ydelta);
+			group.writeClose("MID TRANS");
+		
+
+			//Mapping the closing part
+			ufunc.createFunc(ypos, "+", ypos, ydelta);//Extra space
+			group.writeOpen("ADJUSTMENTS");
+
+			uText ADJUSTHDR = new uText("ADJUSTHDR", outTemp);
+			ADJUSTHDR.createFixed("Adjustments");
+			col1.createMap(ADJUSTHDR, xMid1, ypos);
+			ufunc.createFunc(ypos, "+", ypos, ydelta);
+
+			string[] adjust = { "_HOH PROP CLASS", "_VETERAN PROP CLASS", "_DIS VETERAN PROP CLASS", "_TOTAL ADJ (CALC)" };
+			string[] adjustval = { "_HOH TAXABLE", "_VETERAN TAXABLE", "_DIS VETERAN TAXABLE","_TOTAL ADJ TAXABLE" };//, "_PROPERTY USE"};
+			for (int i = 0; i < outDescr.Length; i++)
+			{
+				uText udescr = new uText(adjust[i], outTemp);
+				uText uVal = new uText(adjustval[i], outTemp);
+
+				test.createCondition(adjust[i], "not EMPTY", true);
+				group.writeOpen("MAP " + adjust[i]);
+	
+				col1.createMap(udescr, xMid1, ypos);
+				col2.createMap(uVal, xMid3, ypos);
+				ufunc.createFunc(ypos, "+", ypos, ydelta);
+				group.writeClose("MAP " + outVal[i]);
+
+			}
+
+			uText ADJUSTTOT = new uText("ADJUST TOTAL", outTemp);
+			ADJUSTHDR.createFixed("Total Adjustments");
+			col1.createMap(ADJUSTTOT, xMid15, ypos);
+			ufunc.createFunc(ypos, "+", ypos, ydelta);
+
+			group.writeClose("ADJUSTMENTS");
+			
+			//tOTAL
+
+			uText total = new uText("TOTAL TOTAL", outTemp);
+			total.createFixed("Net Taxable Value");
+			col1.createMap(total, xMid15, ypos);
+			ufunc.createFunc(ypos, "+", ypos, ydelta);
+
+			da3.mirrorAfterGroup();
 
             outTemp.Close();
         }
+
     }
 
 }
